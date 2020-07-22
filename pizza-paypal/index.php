11433b025738d4a3b0e5658a92e8cbe01a079352
RamiroBenz <ramiro.benzacar@hotmail.com>
2020-07-22 09:16:45 -0300

    Add files via upload

---
+++ pizza-paypal/index.php
@@ -0,0 +1,110 @@
+﻿<html>
+    <head>
+		<meta charset="utf-8">
+        <title>Sistema de pedidos de Pizza con PayPal usando PHP</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="js/jquery.min.js"></script>
+        <!-- jQuery code for implement logic for select pizza size and add toppings checkbox -->
+        <script type="text/javascript">
+            $(document).ready(function () {
+                var size = 0;
+                $('select').change(function () {
+                    size = ($(this).val());
+                    addValue(size);
+                });
+                addValue(size);
+                function addValue(size) {
+                    var total = 0;
+                    function updateTextArea() {
+                        var allVals = [];
+                        $('#checkbox_container :checked').each(function () {
+                            allVals.push($(this).val());
+                        });
+                        if (size === 0) {
+                            size = 10;
+                        }
+                        total = parseFloat(size) + parseFloat(eval(allVals.join("+")));
+                        if (isNaN(total)) {
+                            if (size === 0) {
+                                total = 10;
+                            }
+                            else {
+                                total = size;
+                            }
+                        }
+                        $('p#total span').html(" $ " + total);
+                    }
+                    $(function () {
+                        $('#checkbox_container input').click(updateTextArea);
+                        updateTextArea();
+                    });
+                }
+            });
+        </script>
+    </head>
+    <body style="background-color: #f7f7f7;">
+	 <div id="main">
+			<div class="row">
+				<center><h1 class='text-info'> Sistema de pedidos de Pizza con PayPal usando PHP</h1></center>
+				<div class="col-md-3"></div>
+				<div class="col-md-6">
+				
+					<div class="panel panel-info" style="margin-top: 60px;">
+						<div class="panel-heading"><h3 class='text-center'>Ordenar una Pizza con PayPal</h3></div>
+						  <div class="panel-body">
+							<img id="spizza" src="images/pizza_PNG7135.png" class='img-responsive'/>
+							<form action="process.php" method="post">
+                    <label>Seleccion el tamaño de la Pizza  :</label>
+                    <select name="pizza_type" >
+                        <option value="100">Pizza pequeña - $100.00</option>
+                        <option value="150">Pizza mediana - $150.00</option>
+                        <option value="200">Pizza gigante - $200.00</option>
+                    </select>
+                    <br><br>
+                    <center><h3>Agrega los ingredientes que desees. </h3></center>
+					
+                    <div id="checkbox_container">
+                        <div id="chk">
+                            <p>Jamón</p>
+                            <input class ="chk1" type='checkbox' name='topping1' value='50.00' id="topping1"/><label  class ="chk1" for="topping1"></label> 
+                            <p>$50.00</p>
+                        </div>
+                        <div id="chk">
+                            <p>Pepperoni</p>
+                            <input  class ="chk2" type='checkbox' name='topping2' value='50.00' id="topping2"/><label class ="chk2" for="topping2"></label> 
+                            <p>$50.00</p>
+                        </div>
+
+                        <div id="chk">
+                            <p>Queso</p>
+                            <input  class ="chk4" type='checkbox' name='topping4' value='3.50' id="topping4"/><label class ="chk4" for="topping4"></label> 
+                            <p>$49.99</p>
+                        </div>
+
+                        <div id="chk">
+                            <p>Salami</p>
+                            <input  class ="chk5" type='checkbox' name='topping5' value='50.00' id="topping5"/><label class ="chk5" for="topping5"></label> 
+                            <p>$50.00</p>
+                        </div>
+                        <div id="chk">
+                            <p>Tocino</p>
+                            <input  class ="chk3" type='checkbox' name='topping3' value='50.00' id="topping3"/><label class ="chk3" for="topping3"></label> 
+                            <p>$50.00</p>
+                        </div>
+                    </div>
+                    <center><p id="total" >Monto total a pagar : <span>$ 100 </span></p></center>
+                    <input type="submit" value=" Ordenar ahora " name="submit">
+                </form>
+						  </div>
+					</div>
+				<div class='text-center'>	
+					<img src="images/secure-paypal-logo.jpg" class='text'>
+				</div>	
+				</div>
+			</div>
+		</div>	
+
+    </body>
+</html>
---
+++ pizza-paypal/process.php
@@ -0,0 +1,89 @@
+﻿<?php
+if (isset($_POST['submit'])) {
+// Inicializo las variables para los valores
+$topping1 = 0;
+$topping2 = 0;
+$topping3 = 0;
+$topping4 = 0;
+$topping5 = 0;
+// Inicializo las variables para los nombres
+$topping_name1='';
+$topping_name2='';
+$topping_name3='';
+$topping_name4='';
+$topping_name5='';
+// Agregar URL de paypal ya sea de prueba o modo de produccion 
+$paypal_url = 'https://www.sandbox.paypal.com/cgi-bin/webscr';
+// Añadir correo electrónico del ID del comerciante en PayPal
+$merchant_email = 'joaquinobed123456@gmail.com';
+// Añadir URL de retorno para tu sitio web
+$cancel_return = "http://obedalvarado.pw/demo/pizza-paypal/index.php";
+// Añadir URL de la página de éxito para tu sitio web, esto va a llamar cuando el cliente realiza el proceso de pago en PayPal
+$success_return = "http://obedalvarado.pw/demo/pizza-paypal/success.php";
+$pizza_type = $_POST['pizza_type'];
+if ($pizza_type == 10) {
+$pizza_name = 'Pizza 1';
+$pizza_code="PIZZA001";
+} else if ($pizza_type == 15) {
+$pizza_name = 'Pizza mediana';
+$pizza_code="PIZZA002";
+} else if ($pizza_type == 20) {
+$pizza_name = 'Pizza gigante';
+$pizza_code="PIZZA003";
+}
+if (isset($_POST['topping1'])) {
+$topping1 = 2.00;
+$topping_name1 = ', Jamon';
+}
+if (isset($_POST['topping2'])) {
+$topping2 = 3.00;
+$topping_name2 = ', Pepperoni';
+}
+if (isset($_POST['topping3'])) {
+$topping3 = 2.25;
+$topping_name3 = ', Queso';
+}
+if (isset($_POST['topping4'])) {
+$topping4 = 3.50;
+$topping_name4 = ', Salami';
+}
+if (isset($_POST['topping5'])) {
+$topping5 = 1.25;
+$topping_name5 = ',Tocino';
+}
+$currency = 'USD';//Moneda 
+//Monto total
+$pizza_price = $pizza_type + $topping1 + $topping2 + $topping3 + $topping4 + $topping5;
+
+$pizza_topping_details = "Pizza con ".$topping_name1.$topping_name2.$topping_name3.$topping_name4.$topping_name5;
+
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Procesando con PayPal</title>
+</head>
+<body>
+<div style="margin-left: 38%"><img src="images/ajax-loader.gif"/><img src="images/processing_animation.gif"/></div>
+<form name="myform" action="<?php echo $paypal_url; ?>" method="post" target="_top">
+<input type="hidden" name="cmd" value="_xclick">
+<input type="hidden" name="business" value="<?php echo $merchant_email; ?>">
+<input type="hidden" name="lc" value="C2">
+<input type="hidden" name="item_number" value="<?php echo $pizza_code;?>">
+<input type="hidden" name="item_name" value="<?php echo $pizza_name; ?>">
+<input type="hidden" name="amount" value="<?php echo $pizza_price; ?>">
+<input type="hidden" name="currency_code" value="<?php echo $currency; ?>">
+<input type="hidden" name="button_subtype" value="services">
+<input type="hidden" name="no_note" value="0">
+<input type="hidden" name="on0" value="Ingredientes">
+<input type="hidden" name="os0" value="<?php echo $pizza_topping_details; ?>">
+<input type="hidden" name="cancel_return" value="<?php echo $cancel_return ?>">
+<input type="hidden" name="return" value="<?php echo $success_return; ?>">
+</form>
+<!--Enviando datos a paypal -->
+<script type="text/javascript">
+document.myform.submit();
+</script>
+</body></html>
+<?php } ?>
+
